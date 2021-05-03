fb2dcd1df9fe57e033d57fdbf527454a229faf7b
Nour El Houda Sarmout <nourelhoudasarmout@gmail.com>
2021-05-03 10:18:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 // Add your functions here
 
 
-//const mapToNegativize = (sourceArray) => {}
-map([1,2,3], function(a){ return a * -1 });
+function mapToNegativize(sourceArray) {
+  return sourceArray.map(x => x * -1);
+}
+
+function mapToNoChange(sourceArray) {
+  return sourceArray.map(x => x);
+}
+
+function mapToDouble(sourceArray) {
+  return sourceArray.map(x => x * 2);
+}
+
+function mapToSquare(sourceArray) {
+  return map(sourceArray, function(x){ return x * x })
+}
+
+
+const initialValue = 0;
+
+/* numbers array */
+const numbers = [5, 10, 15];
+
+/* reducer method that takes in the accumulator and next item */
+const reducer = (accumulator, item) => {
+  return accumulator + item;
+};
+
+/* we give the reduce method our reducer function
+  and our initial value */
+const total = numbers.reduce(reducer, initialValue)
